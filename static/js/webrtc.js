d153cccb62d1d5f7b186596c886b99c96945371f
Jisan <jisan789@example.com>
2026-09-15 19:56:32 +0600

    Revert "Redesign app as audio first calling interface"
    This reverts commit b09d9901e20a2f13bbdad4d04e59f0c276518e28.

--- static/js/webrtc.js
+++ static/js/webrtc.js
@@ -92,12 +92,6 @@ class WebRTCManager {
         return videoTracks.length > 0;
     }
 
-    setRemoteAudioEnabled(enabled) {
-        this.remoteAudioElements.forEach(audioElement => {
-            audioElement.volume = enabled ? 1 : 0;
-        });
-    }
-
     hasCamera() {
         return Boolean(this.localStream && this.localStream.getVideoTracks().length);
     }
